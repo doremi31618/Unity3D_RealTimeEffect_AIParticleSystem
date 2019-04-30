65414a3b1034f6b5cfebb4835b7b4d03372eeae5
AlexxxxxLu <46194292+AlexxxxxLu@users.noreply.github.com>
2019-04-30 15:43:33 +0800

    Alex

--- Assets/Script/Boundary/BoundaryEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class BoundaryEvent
-{
-    public abstract void StayBoundary();
-    public abstract void ExitBoundary();
-}
