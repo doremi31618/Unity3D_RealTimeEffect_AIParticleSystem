f2a5cb67425bd5db835034980d687b26bc30a88d
ericZhan <dablog@gmail.com>
2019-06-13 20:24:45 +0800

    update revert

--- Assets/Scenes/SceneTransferTest/StageTransferTool.cs
+++ Assets/Scenes/SceneTransferTest/StageTransferTool.cs
@@ -67,6 +67,7 @@ public class StageTransferTool : MonoBehaviour
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 
 <<<<<<< HEAD
         if(Input.GetKeyDown(KeyCode.Space))
@@ -80,6 +81,8 @@ public class StageTransferTool : MonoBehaviour
 >>>>>>> parent of f69532e1... Merge branch 'master' of https://github.com/doremi31618/AIParticleSystem
 =======
 >>>>>>> parent of 1c5d5b50... update
+=======
+>>>>>>> parent of 7127eb91... update
 =======
 >>>>>>> parent of 7127eb91... update
     }
